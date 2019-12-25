d57433ec33272c38d4ecc53246383edadc02c6bd
Липская Виктория <yuliya.santotsckay@yandex.ru>
2019-12-26 01:33:43 +0200

    nv

---
+++ Class2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+//using System;
+using Android.Hardware;
+using Context = Android.Content.Context;
+//using System.Linq;
+
+namespace App3
+{
+    [Activity(Label="Class2",Theme = "@style/AppTheme", MainLauncher = true)]
+    class Class2 : AppCompatActivity
+    {
+        protected Button back;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.layout2);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);//?
+
+            back = FindViewById<Button>(Resource.Id.buttonback);//Инициализация кнопки для показа другой активити
+            
+            back.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+
+            };
+        }
+    }
+
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -7,30 +7,39 @@ using System;
 using Android.Hardware;
 using Context = Android.Content.Context;
 using System.Linq;
-
+using Android.Content;
 
 namespace App3
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
      public class MainActivity : AppCompatActivity, ISensorEventListener
     {
+        double dt; // отрезое между снятіем ускоренія в 2 точках
+        double allt;//все время 
+        long lasttime;
+        readonly double[] v= new double [3]; //скорость
+        readonly double[] dr= new double [3];  //перемеўеніе
 
         protected SensorManager msensorManager;//Менеджер сенсоров аппрата
         //private final Sensor mAccelerometer;
+       //readonly string [] x;
         private float[] accelData;
         protected Button start;
         protected Button stop;
-        /*
-        private EditText xView;
-        private EditText yView;
-        private EditText zView;
-        */
+        protected Button show;
+
         private TextView xView;
         private TextView yView;
         private TextView zView;
-        private TextView textch;
+
+        private TextView vx;
+        private TextView vy;
+        private TextView vz;
+
+        //  private TextView textch;
         protected override void OnCreate(Bundle savedInstanceState)
         {
+         
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -40,23 +49,44 @@ namespace App3
             yView = (TextView)FindViewById(Resource.Id.textView5);  // текстовые поля для вывода показаний
             zView = (TextView)FindViewById(Resource.Id.textView6);  //
 
-            start = FindViewById<Button>(Resource.Id.button11);//ІНІЦІАЛІЗАЦІЯ
-            stop = FindViewById<Button>(Resource.Id.button2);//ІНІЦІАЛІЗАЦІЯ
+            vx= (TextView)FindViewById(Resource.Id.textView10);
+            vy = (TextView)FindViewById(Resource.Id.textView11);
+            vz = (TextView)FindViewById(Resource.Id.textView12);
 
-            textch = (TextView)FindViewById(Resource.Id.textView7);
+            start = FindViewById<Button>(Resource.Id.button11);//Инициализация кнопки старт
+            stop = FindViewById<Button>(Resource.Id.button2);//Инициализация кнопки стоп
+            show = FindViewById<Button>(Resource.Id.button12);//Инициализация кнопки для показа другой активити
+
+            // textch = (TextView)FindViewById(Resource.Id.textView7);
             start.Click += delegate (object sender, EventArgs e)//
               {
-                  textch.Text = "Идет снятие показаний...";
-                  start.Text = "Идет снятие показаний..."; 
+                //  textch.Text = "Идет снятие показаний...";
+                  start.Text = "Cнятие показаний...";             
               };
-
+           
             stop.Click += delegate (object sender, EventArgs e)//new
             {
                 start.Text = "START";
+                dr[0] = 0;
+                dr[1] = 0;
+                dr[2] = 0;
+                v[0] = 0;
+                v[1] = 0;
+                v[2] = 0;
+                allt = 0;
+
+
                 ////  start.SetTextSize = "15dp";
-              //  float size = 15;
-              // void start.SetTextSize(size);
-                textch.Text = "Нажмите START для получения координат ";
+                //  float size = 15;
+                // void start.SetTextSize(size);
+
+                // textch.Text = "Нажмите START для получения координат ";
+            };
+            //для подключенія через кнопку новой активити
+            show.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(Class2));
+                StartActivity(intent);             
             };
         }
         //@Override
@@ -69,33 +99,43 @@ namespace App3
 
         //Чтобы наша программа не съедала ресурсы смартфона будучи свернутой по событию onPause
         //«снимаем» получение данных с датчиков:
-       
-            //    @Override
-            /* override protected void OnPause()
-             {
-                 base.OnPause();
-                 // this.OnPause();
 
-                 ISensorEventListener unlisten;
-               //  ISensorEventListener listener = null;
-                 msensorManager.UnregisterListener(unlisten, msensorManager.GetDefaultSensor(SensorType.Accelerometer), SensorDelay.Normal);
-             }*/
+        //    @Override
+        /*    override protected void OnPause()
+            {
+                base.OnPause();
+                // this.OnPause();
 
-            //создадим новый метод, в котором данные с датчиков будем заносить в соответствующий 
-            //датчику массив. Назовем метод loadNewSensorData:
-            private void LoadNewSensorData(SensorEvent e) 
-        {
-            /*final*/ var type = e.Sensor.Type; //Определяем тип датчика
-            if ( type == SensorType.Accelerometer) 
-            //Если акселерометр
-            { 
-                accelData = e.Values.ToArray();
+                msensorManager.UnregisterListener(this, msensorManager.GetDefaultSensor(SensorType.Accelerometer), SensorDelay.Game);
             }
-       
-       // if (type == Sensor.TYPE_MAGNETIC_FIELD) { //Если геомагнитный датчик
-       //     magnetData = event.values.clone();
-    }
-       
+            */
+
+        //создадим новый метод, в котором данные с датчиков будем заносить в соответствующий 
+        //датчику массив. Назовем метод loadNewSensorData:
+        private void LoadNewSensorData(SensorEvent e)
+        {
+            var type = e.Sensor.Type; //Определяем тип датчика
+            if (type == SensorType.Accelerometer)
+            //Если акселерометр
+            {
+                accelData = e.Values.ToArray();
+               
+                //Integrirovanie(accelData);
+                dt = (e.Timestamp - lasttime) * 1e-9;
+                lasttime = e.Timestamp;
+
+                allt += dt;   //все время от нажатия на сброс
+                v[0] += accelData[0] * dt;
+                v[1] += accelData[1] * dt;
+                v[2] += accelData[2] * dt;
+
+                dr[0] += v[0] * dt;
+                dr[1] += v[1] * dt;
+                dr[2] += v[2] * dt;
+            }
+        }
+   
+
         public void OnSensorChanged(SensorEvent e)
         {
             LoadNewSensorData(e); // Получаем данные с датчика
@@ -107,15 +147,53 @@ namespace App3
                 xView = (TextView)FindViewById(Resource.Id.textView4);
                 yView = (TextView)FindViewById(Resource.Id.textView5);
                 zView = (TextView)FindViewById(Resource.Id.textView6);
-            }   
+            }
+            //textview 10,11,12
+
+                vx = (TextView)FindViewById(Resource.Id.textView10);
+                vy = (TextView)FindViewById(Resource.Id.textView11);
+                vz = (TextView)FindViewById(Resource.Id.textView12);
             //Выводим результат
+        
             xView.Text = (accelData[0]).ToString();
             yView.Text = (accelData[1]).ToString();
-            zView.Text = (accelData[2]).ToString();      
-        }
+            zView.Text = (accelData[2]).ToString();
 
+            vx.Text = (v[0]).ToString();
+            vy.Text = (v[1]).ToString();
+            vz.Text = (v[2]).ToString();
+
+
+          //  vx.Text = (accelData[0]).ToString();
+
+        }
+       
+        //int c;
+ 
+       /* private void Button1_Click(object sender, EventArgs e)//для стоп
+        {
+            c++;
+            stop.Text = Convert.ToString(c);
+         
+        }*/
         public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
         {
         }
+
     }
 }
+
+/*
+
+    <TextView
+        android:text= "Нажмите START для получения координат :"
+        android:textColor="@color/abc_background_cache_hint_selector_material_dark"
+        android:layout_width="363.0dp"
+        android:layout_height="74.5dp"
+        android:textSize = "19dp"
+        android:layout_marginLeft="0.0dp"
+        android:id="@+id/textView7"
+        android:layout_marginBottom="0.0dp"
+        android:layout_marginTop="0.0dp"
+        android:layout_marginRight="17.0dp" />
+        */
