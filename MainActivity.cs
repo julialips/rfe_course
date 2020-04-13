ddfd13b721650188d617f318e375b768a6daff8d
mi <mi@DESKTOP-K6I79MJ>
2020-04-13 22:45:11 +0300

    комментарии

--- MainActivity.cs
+++ MainActivity.cs
@@ -8,7 +8,7 @@ using Android.Hardware;
 using Context = Android.Content.Context;
 using System.Linq;
 using Android.Content;
-//using ZedGraph;
+
 
 
 namespace App3
@@ -17,16 +17,16 @@ namespace App3
      public class MainActivity : AppCompatActivity, ISensorEventListener
     {
         
-        double dt; // отрезое между снятіем ускоренія в 2 точках
-        double allt;//все время 
+        double dt; // отрезое между снятием ускорения в 2 точках
+        double allt; //все время 
         long lasttime;
         readonly double[] v= new double [3]; //скорость
-        readonly double[] dr= new double [3];  //перемеўеніе
+        readonly double[] dr= new double [3];  //перемемещение
 
-        protected SensorManager msensorManager;//Менеджер сенсоров аппрата
+        protected SensorManager msensorManager; //Менеджер сенсоров 
         //private final Sensor mAccelerometer;
-       //readonly string [] x;
-        private float[] accelData;
+        //readonly string [] x;
+        private float[] accelData; // массив ускорений по 3-м осям в формате xyzxyz...
         protected Button start;
         protected Button stop;
         protected Button show;
@@ -43,7 +43,6 @@ namespace App3
         private TextView dry;
         private TextView drz;
 
-        //  private TextView textch;
         protected override void OnCreate(Bundle savedInstanceState)
         {
          
@@ -51,35 +50,35 @@ namespace App3
             SetContentView(Resource.Layout.activity_main);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             msensorManager = (SensorManager)GetSystemService(Context.SensorService);
+
             accelData = new float[3];
             xView = (TextView)FindViewById(Resource.Id.textView4);  //
-            yView = (TextView)FindViewById(Resource.Id.textView5);  // текстовые поля для вывода показаний
+            yView = (TextView)FindViewById(Resource.Id.textView5);  // текстовые поля для вывода показаний ускорений
             zView = (TextView)FindViewById(Resource.Id.textView6);  //
 
             vx = (TextView)FindViewById(Resource.Id.textView10);
-            vy = (TextView)FindViewById(Resource.Id.textView11);
+            vy = (TextView)FindViewById(Resource.Id.textView11);   // поля для значений скоростей
             vz = (TextView)FindViewById(Resource.Id.textView12);
 
             drx = (TextView)FindViewById(Resource.Id.textView14);
-            dry = (TextView)FindViewById(Resource.Id.textView22);
+            dry = (TextView)FindViewById(Resource.Id.textView22);  // поля для значений перемещений
             drz = (TextView)FindViewById(Resource.Id.textView24);
 
 
-
-
-            start = FindViewById<Button>(Resource.Id.button11);//Инициализация кнопки старт
-            stop = FindViewById<Button>(Resource.Id.button2);//Инициализация кнопки стоп
-            show = FindViewById<Button>(Resource.Id.button12);//Инициализация кнопки для показа другой активити
+            start = FindViewById<Button>(Resource.Id.button11); //Инициализация кнопки старт
+            stop = FindViewById<Button>(Resource.Id.button2);   //Инициализация кнопки стоп
+            show = FindViewById<Button>(Resource.Id.button12);  //Инициализация кнопки для перехода в другую активити(страницу)
 
             // textch = (TextView)FindViewById(Resource.Id.textView7);
-            start.Click += delegate (object sender, EventArgs e)//
-              {
-                //  textch.Text = "Идет снятие показаний...";
-                  start.Text = "Running...";             
+            start.Click += delegate (object sender, EventArgs e)
+              {     
+                  start.Text = "Running...";  
+                  //кнопка просто меняет текст, никакого другого смысла в ней, к сожалению, не реализовано
               };
            
-            stop.Click += delegate (object sender, EventArgs e)//new
+            stop.Click += delegate (object sender, EventArgs e)
             {
+                // на кнопку Стоп происходит обнуление накопленных значение по скорости и перемещению
                 start.Text = "START";
                 dr[0] = 0;
                 dr[1] = 0;
@@ -88,15 +87,9 @@ namespace App3
                 v[1] = 0;
                 v[2] = 0;
                 allt = 0;
-
-
-                ////  start.SetTextSize = "15dp";
-                //  float size = 15;
-                // void start.SetTextSize(size);
-
-                // textch.Text = "Нажмите START для получения координат ";
             };
-            //для подключенія через кнопку новой активити
+
+            //для подключения через кнопку новой активити
             show.Click += delegate
             {
                 Intent intent = new Intent(this, typeof(Class2));
@@ -134,7 +127,7 @@ namespace App3
             {
                 accelData = e.Values.ToArray();
                
-                //Integrirovanie(accelData);
+                //Получение времени Integrirovanie(accelData);
                 dt = (e.Timestamp - lasttime) * 1e-9;
                 lasttime = e.Timestamp;
 
@@ -156,13 +149,13 @@ namespace App3
            // SensorManager.GetRotationMatrix(rotationMatrix, null, accelData, magnetData); //Получаем матрицу поворота
             //SensorManager.GetOrientation(rotationMatrix, OrientationData); //Получаем данные ориентации устройства в пространстве
          
+            //заполняем поля соответствующими значениями
             if ((xView == null) || (yView == null) || (zView == null))
-            {  //Без этого работать отказалось.
+            {  
                 xView = (TextView)FindViewById(Resource.Id.textView4);
                 yView = (TextView)FindViewById(Resource.Id.textView5);
                 zView = (TextView)FindViewById(Resource.Id.textView6);
             }
-            //textview 10,11,12
 
                 vx = (TextView)FindViewById(Resource.Id.textView10);
                 vy = (TextView)FindViewById(Resource.Id.textView11);
@@ -180,40 +173,11 @@ namespace App3
             drx.Text = (dr[0]).ToString();
             dry.Text = (dr[1]).ToString();
             drz.Text = (dr[2]).ToString();
-
-            //
-           // Chart myChart = new Chart();
-            //GraphPane pane = zedGraph.GraphPane;
-
         }
        
-
-                //int c;
-
-                /* private void Button1_Click(object sender, EventArgs e)//для стоп
-                 {
-                     c++;
-                     stop.Text = Convert.ToString(c);
-
-                 }*/
-                public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
-        {
-        }
+            public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        { }
 
     }
 }
 
-/*
-
-    <TextView
-        android:text= "Нажмите START для получения координат :"
-        android:textColor="@color/abc_background_cache_hint_selector_material_dark"
-        android:layout_width="363.0dp"
-        android:layout_height="74.5dp"
-        android:textSize = "19dp"
-        android:layout_marginLeft="0.0dp"
-        android:id="@+id/textView7"
-        android:layout_marginBottom="0.0dp"
-        android:layout_marginTop="0.0dp"
-        android:layout_marginRight="17.0dp" />
-        */
