76d4f626d130b75052550fbcd8cf979d79cbe6f1
mi <mi@DESKTOP-K6I79MJ>
2020-05-07 23:40:48 +0300

    Added Russian comments, display values quaternions on the screen

--- MainActivity.cs
+++ MainActivity.cs
@@ -19,14 +19,14 @@ using Xamarin.Essentials;
 namespace App3
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-     public class MainActivity : AppCompatActivity, ISensorEventListener
+    public class MainActivity : AppCompatActivity, ISensorEventListener
     {
-        
+
         double dt; // отрезое между снятием ускорения в 2 точках
         double allt; //все время 
         long lasttime;
-        readonly double[] v= new double [3]; //скорость
-        readonly double[] dr= new double [3];  //перемемещение
+        readonly double[] v = new double[3]; //скорость
+        readonly double[] dr = new double[3];  //перемемещение
 
         protected SensorManager msensorManager; //Менеджер сенсоров 
         //private final Sensor mAccelerometer;
@@ -53,9 +53,11 @@ namespace App3
         private TextView dry;
         private TextView drz;
 
+        public TextView QuaterionField;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-         
+
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -74,6 +76,7 @@ namespace App3
             dry = (TextView)FindViewById(Resource.Id.textView22);  // поля для значений перемещений
             drz = (TextView)FindViewById(Resource.Id.textView24);
 
+            QuaterionField = (TextView)FindViewById(Resource.Id.textView_Value_Quaterion);
 
             start = FindViewById<Button>(Resource.Id.button11); //Инициализация кнопки старт
             stop = FindViewById<Button>(Resource.Id.button2);   //Инициализация кнопки стоп
@@ -81,11 +84,11 @@ namespace App3
 
             // textch = (TextView)FindViewById(Resource.Id.textView7);
             start.Click += delegate (object sender, EventArgs e)
-              {     
-                  start.Text = "Running...";  
+              {
+                  start.Text = "Running...";
                   //кнопка просто меняет текст, никакого другого смысла в ней, к сожалению, не реализовано
               };
-           
+
             stop.Click += delegate (object sender, EventArgs e)
             {
                 // на кнопку Стоп происходит обнуление накопленных значение по скорости и перемещению
@@ -103,14 +106,14 @@ namespace App3
             show.Click += delegate
             {
                 Intent intent = new Intent(this, typeof(Class2));
-                StartActivity(intent);             
+                StartActivity(intent);
             };
         }
         //@Override
         override protected void OnResume()
         {
             base.OnResume();
-            msensorManager.RegisterListener(this, msensorManager.GetDefaultSensor(SensorType.Accelerometer),SensorDelay.Game);
+            msensorManager.RegisterListener(this, msensorManager.GetDefaultSensor(SensorType.Accelerometer), SensorDelay.Game);
             // 05.05
             msensorManager.RegisterListener(this, msensorManager.GetDefaultSensor(SensorType.Gyroscope), SensorDelay.Game);
             msensorManager.RegisterListener(this, msensorManager.GetDefaultSensor(SensorType.MagneticField), SensorDelay.Game);
@@ -135,62 +138,69 @@ namespace App3
         //датчику массив. Назовем метод loadNewSensorData:
         private void LoadNewSensorData(SensorEvent e)
         {
-            var type = e.Sensor.Type; //Определяем тип датчика
+            //Определяем тип датчика
+            var type = e.Sensor.Type;
             if (type == SensorType.Accelerometer)
             //Если акселерометр
             {
+                //Записываем данные с датчика в массив
                 accelData = e.Values.ToArray();
 
                 //Получение времени Integrirovanie(accelData);
                 dt = (e.Timestamp - lasttime) * 1e-9;
+                //время между двумя последними событиями(снятиями показаний с датчика)
                 lasttime = e.Timestamp;
 
-                allt += dt;   //все время от нажатия на сброс
+                //все время от нажатия на сброс
+                allt += dt;
+                //первое интегрирование, получение скорости
                 v[0] += accelData[0] * dt;
                 v[1] += accelData[1] * dt;
                 v[2] += accelData[2] * dt;
 
+                //второе интегрирование, получение перемещения по каждой из координат
                 dr[0] += v[0] * dt;
                 dr[1] += v[1] * dt;
                 dr[2] += v[2] * dt;
             }
 
             //05.05
-            var typee = e.Sensor.Type; //Определяем тип датчика
+            //Определяем тип датчика, если гироскоп
+            var typee = e.Sensor.Type;
             if (typee == SensorType.Gyroscope)
-            //Если акселерометр
             {
+                //Записываем данные с гироскопа в массив
                 giroscopeData = e.Values.ToArray();
             }
 
-            var type_mag = e.Sensor.Type; //Определяем тип датчика
+            //Определяем тип датчика, если магнетометр
+            var type_mag = e.Sensor.Type;
             if (type_mag == SensorType.MagneticField)
-            //Если акселерометр
             {
                 magnitometrData = e.Values.ToArray();
             }
             //05.05
-
         }
         public void OnSensorChanged(SensorEvent e)
         {
-            LoadNewSensorData(e); // Получаем данные с датчика
-           // SensorManager.GetRotationMatrix(rotationMatrix, null, accelData, magnetData); //Получаем матрицу поворота
-            //SensorManager.GetOrientation(rotationMatrix, OrientationData); //Получаем данные ориентации устройства в пространстве
-         
-            //заполняем поля соответствующими значениями
-            if ((xView == null) || (yView == null) || (zView == null))
-            {  
-                xView = (TextView)FindViewById(Resource.Id.textView4);
-                yView = (TextView)FindViewById(Resource.Id.textView5);
-                zView = (TextView)FindViewById(Resource.Id.textView6);
-            }
+            // Получаем данные с датчика
+            LoadNewSensorData(e);
+            // SensorManager.GetRotationMatrix(rotationMatrix, null, accelData, magnetData); //Получаем матрицу поворота
+            // SensorManager.GetOrientation(rotationMatrix, OrientationData); //Получаем данные ориентации устройства в пространстве
 
-                vx = (TextView)FindViewById(Resource.Id.textView10);
-                vy = (TextView)FindViewById(Resource.Id.textView11);
-                vz = (TextView)FindViewById(Resource.Id.textView12);
+            //заполняем поля полученными с датчиков значениями
+            //if ((xView == null) || (yView == null) || (zView == null))
+            //{  
+            // xView = (TextView)FindViewById(Resource.Id.textView4);
+            //  yView = (TextView)FindViewById(Resource.Id.textView5); уже определены вначале
+            //  zView = (TextView)FindViewById(Resource.Id.textView6);
+            // }
+
+            //  vx = (TextView)FindViewById(Resource.Id.textView10);
+            //  vy = (TextView)FindViewById(Resource.Id.textView11);
+            // vz = (TextView)FindViewById(Resource.Id.textView12);
             //Выводим результат
-        
+
             xView.Text = (accelData[0]).ToString();
             yView.Text = (accelData[1]).ToString();
             zView.Text = (accelData[2]).ToString();
@@ -203,11 +213,15 @@ namespace App3
             dry.Text = (dr[1]).ToString();
             drz.Text = (dr[2]).ToString();
             // 06.05
+            //создание объекта класса фильтра Маджвика
             MadgwickAHRS madgwick = new MadgwickAHRS(1f / 256f);
             madgwick.Update(deg2rad(giroscopeData[0]), deg2rad(giroscopeData[1]), deg2rad(giroscopeData[2]), accelData[0], accelData[1], accelData[2]);
-            
 
-            static float deg2rad(float degrees)//преобразование кватерионов в углы эйлера
+            //выводим в текстовое поле значение кватерниона из свойства класса MadgwickAHRS {get;set}
+            QuaterionField.Text = (madgwick.Quaternion).ToString();
+
+            //преобразование кватерyионов в углы эйлера
+            static float deg2rad(float degrees)
             {
                 return (float)(Math.PI / 180) * degrees;
             }
@@ -220,40 +234,40 @@ namespace App3
         // new code 05.05
         //
         //
-        public class MadgwickAHRS
-        {
-         
-            /// <summary>
+        //07.05//
+    }
+       //07.05//
+    public class MadgwickAHRS
+        {       
             /// Gets or sets the sample period.
-            /// </summary>
-            public float SamplePeriod { get; set; }
+           public float SamplePeriod { get; set; }
 
-            /// <summary>
             /// Gets or sets the algorithm gain beta.
-            /// </summary>
             public float Beta { get; set; }
 
-            /// <summary>
             /// Gets or sets the Quaternion output.
-            /// </summary>
-            public float[] Quaternion { get; set; }
+            // public float[] Quaternion { get; set; } так в оригинате, 07.05
 
-            /// <summary>
-            /// Initializes a new instance of the <see cref="MadgwickAHRS"/> class.
-            /// </summary>
-            /// <param name="samplePeriod">
-            /// Sample period.
-            /// </param>
-            public MadgwickAHRS(float samplePeriod)
-                : this(samplePeriod, 1f)
+            public float[] Quaternion 
             {
+                get { return Quaternion; }//так я сделала, по сути просто явно определила, кажется это не обязаельно
+                set { }
             }
 
             /// <summary>
-            /// Initializes a new instance of the <see cref="MadgwickAHRS"/> class.
+            /// Инициализация нового экземпляра класса <see cref="MadgwickAHRS"/> 
             /// </summary>
             /// <param name="samplePeriod">
-            /// Sample period.
+            /// Период выборки
+            /// </param>
+            public MadgwickAHRS(float samplePeriod): this(samplePeriod, 1f)// этот я использую
+            { }
+
+            /// <summary>
+            /// Инициализация нового экземпляра класса <see cref="MadgwickAHRS"/> 
+            /// </summary>
+            /// <param name="samplePeriod">
+            /// Период выборки.
             /// </param>
             /// <param name="beta">
             /// Algorithm gain beta.
@@ -295,22 +309,20 @@ namespace App3
             /// <param name="mz">
             /// Magnetometer z axis measurement in any calibrated units.
             /// </param>
-            /// <remarks>
             /// Optimised for minimal arithmetic.
             /// Total ±: 160
             /// Total *: 172
             /// Total /: 5
             /// Total sqrt: 5
-            /// </remarks> 
             public void Update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
             {
-                float q1 = Quaternion[0], q2 = Quaternion[1], q3 = Quaternion[2], q4 = Quaternion[3];   // short name local variable for readability
+                float q1 = Quaternion[0], q2 = Quaternion[1], q3 = Quaternion[2], q4 = Quaternion[3];   
                 float norm;
                 float hx, hy, _2bx, _2bz;
                 float s1, s2, s3, s4;
                 float qDot1, qDot2, qDot3, qDot4;
 
-                // Auxiliary variables to avoid repeated arithmetic
+            // Вспомогательные переменные, чтобы избежать повторной арифметики
                 float _2q1mx;
                 float _2q1my;
                 float _2q1mz;
@@ -334,15 +346,15 @@ namespace App3
                 float q3q4 = q3 * q4;
                 float q4q4 = q4 * q4;
 
-                // Normalise accelerometer measurement
+                // Нормализация измерений акселерометра
                 norm = (float)Math.Sqrt(ax * ax + ay * ay + az * az);
                 if (norm == 0f) return; // handle NaN
                 norm = 1 / norm;        // use reciprocal for division
                 ax *= norm;
                 ay *= norm;
                 az *= norm;
-
-                // Normalise magnetometer measurement
+ 
+                // Нормализация измерений магнетометра
                 norm = (float)Math.Sqrt(mx * mx + my * my + mz * mz);
                 if (norm == 0f) return; // handle NaN
                 norm = 1 / norm;        // use reciprocal for division
@@ -350,7 +362,7 @@ namespace App3
                 my *= norm;
                 mz *= norm;
 
-                // Reference direction of Earth's magnetic field
+               // Контрольное направление магнитного поля Земли
                 _2q1mx = 2f * q1 * mx;
                 _2q1my = 2f * q1 * my;
                 _2q1mz = 2f * q1 * mz;
@@ -362,8 +374,8 @@ namespace App3
                 _4bx = 2f * _2bx;
                 _4bz = 2f * _2bz;
 
-                // Gradient decent algorithm corrective step
-                s1 = -_2q3 * (2f * q2q4 - _2q1q3 - ax) + _2q2 * (2f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+                // Метод градиентного спуска
+                 s1 = -_2q3 * (2f * q2q4 - _2q1q3 - ax) + _2q2 * (2f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
                 s2 = _2q4 * (2f * q2q4 - _2q1q3 - ax) + _2q1 * (2f * q1q2 + _2q3q4 - ay) - 4f * q2 * (1 - 2f * q2q2 - 2f * q3q3 - az) + _2bz * q4 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
                 s3 = -_2q1 * (2f * q2q4 - _2q1q3 - ax) + _2q4 * (2f * q1q2 + _2q3q4 - ay) - 4f * q3 * (1 - 2f * q2q2 - 2f * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
                 s4 = _2q2 * (2f * q2q4 - _2q1q3 - ax) + _2q3 * (2f * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
@@ -373,18 +385,20 @@ namespace App3
                 s3 *= norm;
                 s4 *= norm;
 
-                // Compute rate of change of quaternion
+                // Вычисление скорости изменения кватерниона
                 qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - Beta * s1;
                 qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - Beta * s2;
                 qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - Beta * s3;
                 qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - Beta * s4;
 
-                // Integrate to yield quaternion
+                // Интегрирование, для получения кватерниона
                 q1 += qDot1 * SamplePeriod;
                 q2 += qDot2 * SamplePeriod;
                 q3 += qDot3 * SamplePeriod;
                 q4 += qDot4 * SamplePeriod;
-                norm = 1f / (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
+                norm = 1f / (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // нормализация кватерниона
+                
+
                 Quaternion[0] = q1 * norm;
                 Quaternion[1] = q2 * norm;
                 Quaternion[2] = q3 * norm;
@@ -412,21 +426,17 @@ namespace App3
             /// <param name="az">
             /// Accelerometer z axis measurement in any calibrated units.
             /// </param>
-            /// <remarks>
-            /// Optimised for minimal arithmetic.
-            /// Total ±: 45
-            /// Total *: 85
-            /// Total /: 3
-            /// Total sqrt: 3
-            /// </remarks>
+
+            /// Optimised for minimal arithmetic. Total ±: 45. Total *: 85. Total /: 3. Total sqrt: 3
+
             public void Update(float gx, float gy, float gz, float ax, float ay, float az)
             {
-                float q1 = Quaternion[0], q2 = Quaternion[1], q3 = Quaternion[2], q4 = Quaternion[3];   // short name local variable for readability
+                float q1 = Quaternion[0], q2 = Quaternion[1], q3 = Quaternion[2], q4 = Quaternion[3];   
                 float norm;
                 float s1, s2, s3, s4;
                 float qDot1, qDot2, qDot3, qDot4;
 
-                // Auxiliary variables to avoid repeated arithmetic
+                // Вспомогательные переменные, чтобы избежать повторной арифметики
                 float _2q1 = 2f * q1;
                 float _2q2 = 2f * q2;
                 float _2q3 = 2f * q3;
@@ -441,7 +451,7 @@ namespace App3
                 float q3q3 = q3 * q3;
                 float q4q4 = q4 * q4;
 
-                // Normalise accelerometer measurement
+                // Нормализация измерений акселерометра
                 norm = (float)Math.Sqrt(ax * ax + ay * ay + az * az);
                 if (norm == 0f) return; // handle NaN
                 norm = 1 / norm;        // use reciprocal for division
@@ -449,7 +459,7 @@ namespace App3
                 ay *= norm;
                 az *= norm;
 
-                // Gradient decent algorithm corrective step
+                // Метод градиентного спуска
                 s1 = _4q1 * q3q3 + _2q3 * ax + _4q1 * q2q2 - _2q2 * ay;
                 s2 = _4q2 * q4q4 - _2q4 * ax + 4f * q1q1 * q2 - _2q1 * ay - _4q2 + _8q2 * q2q2 + _8q2 * q3q3 + _4q2 * az;
                 s3 = 4f * q1q1 * q3 + _2q1 * ax + _4q3 * q4q4 - _2q4 * ay - _4q3 + _8q3 * q2q2 + _8q3 * q3q3 + _4q3 * az;
@@ -460,18 +470,20 @@ namespace App3
                 s3 *= norm;
                 s4 *= norm;
 
-                // Compute rate of change of quaternion
+                // Вычисление скорости изменения кватерниона
                 qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - Beta * s1;
                 qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - Beta * s2;
                 qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - Beta * s3;
                 qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - Beta * s4;
 
-                // Integrate to yield quaternion
+                //  Интегрирование для получения кватерниона
                 q1 += qDot1 * SamplePeriod;
                 q2 += qDot2 * SamplePeriod;
                 q3 += qDot3 * SamplePeriod;
                 q4 += qDot4 * SamplePeriod;
-                norm = 1f / (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
+
+                //нормализация кватерниона
+                norm = 1f / (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    
                 Quaternion[0] = q1 * norm;
                 Quaternion[1] = q2 * norm;
                 Quaternion[2] = q3 * norm;
@@ -484,8 +496,6 @@ namespace App3
         //
         //
 
-
-
-    }
+   // } 07.05
 }
 
